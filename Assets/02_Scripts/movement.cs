c8033db1f95a33cb97ede5dc90614546697bc330
Elija <eli.schmuck@gmx.net>
2024-11-09 09:10:49 +0100

    movement edit

--- Assets/02_Scripts/movement.cs
+++ Assets/02_Scripts/movement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour,i_Update
 {
-    public float moveSpeed = 5f;          // Geschwindigkeit des Spielers
+    public float speed = 5f;          // Geschwindigkeit des Spielers
     private Rigidbody2D rb;               // Referenz zum Rigidbody2D des Spielers
     private Vector2 movement;             // Bewegungsvektor
     
@@ -16,15 +16,20 @@ public class PlayerMovement : MonoBehaviour,i_Update
 
    public void CostumUpdate()
     {
-        // Eingaben sammeln
-
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        //moveSpeed
+        float horizontalInput = Input.GetAxisRaw("Horizontal"); 
+        float vertiacalInput = Input.GetAxisRaw("Vertical");
+        
+        Vector3 inputVector = new Vector3(horizontalInput, vertiacalInput, 0); 
+        float inputStrengthClamped = Mathf.Min(inputVector.magnitude, 1); 
+        inputVector = inputVector.normalized * inputStrengthClamped;
+        transform.position += inputVector * Time.deltaTime * speed;
+      
     }
 
     void FixedUpdate()
     {
         // Bewegung anwenden
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
     }
 }
