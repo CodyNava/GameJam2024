b07b0d8c1d0fe4d5a4e81efe677ce0b49672917f
Grorhall <167302118+Grorhall@users.noreply.github.com>
2024-11-09 19:28:37 +0100

    Changed Shapeshift script

--- Assets/02_Scripts/ShapeShifting.cs
+++ Assets/02_Scripts/ShapeShifting.cs
@@ -1,71 +1,46 @@
 using UnityEngine;
-
-//Character Forms
-
-using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
-public class FormChanger : MonoBehaviour
+public class ShapeShifting : MonoBehaviour, i_Update
 {
+    private void Start() { UpdateManager.Instance.RegisterUpdate(this); ActivateForm(currentFormIndex); }
+    private void OnDisable() { UpdateManager.Instance.UnregisterUpdate(this); }
+
     public GameObject[] forms; // Array für die 4 Formen
+    public Slider[] cooldownSlider;
     private int currentFormIndex = 0; // Aktuelle Form-Index
     private bool canChangeForm = true; // Cooldown-Kontrolle
     private float cooldownDuration = 5f; // Cooldown in Sekunden
 
-    void Start()
+    public void CustomUpdate()
     {
-        ActivateForm(currentFormIndex);
-    }
+        if (BoolControler.Instance.isNormal) { NormalFormEnabled(); }
+        if (BoolControler.Instance.isFire) { FireFormEnabled(); }
+        if (BoolControler.Instance.isWater) { WaterFormEnabled(); }
+        if (BoolControler.Instance.isNature) { NatureFormEnabled(); }
 
-    void Update()
-    {
+
+        float dpadHorizontal = Input.GetAxis("DPadHorizontal");
+        float dpadVertical = Input.GetAxis("DPadVertical");
+
         if (canChangeForm)
-        {
+        {    /*
+            if (Input.GetKeyDown(KeyCode.Alpha1) || (dpadHorizontal == -1)) ChangeForm(0);
+            else if (Input.GetKeyDown(KeyCode.Alpha2) || (dpadVertical == 1)) ChangeForm(1);
+            else if (Input.GetKeyDown(KeyCode.Alpha3) || (dpadHorizontal == 1)) ChangeForm(2);
+            else if (Input.GetKeyDown(KeyCode.Alpha4) || (dpadVertical == -1)) ChangeForm(3);*/
+
             if (Input.GetKeyDown(KeyCode.Alpha1)) ChangeForm(0);
             else if (Input.GetKeyDown(KeyCode.Alpha2)) ChangeForm(1);
             else if (Input.GetKeyDown(KeyCode.Alpha3)) ChangeForm(2);
             else if (Input.GetKeyDown(KeyCode.Alpha4)) ChangeForm(3);
+
         }
     }
-     
-    void ChangeForm(int formIndex)
+    public void NormalFormEnabled()
     {
-        if (formIndex != currentFormIndex)
-        {
-            forms[formIndex].gameObject.SetActive(true);
-          
-            forms[currentFormIndex].gameObject.SetActive(false);
-            currentFormIndex = formIndex;
-            ActivateForm(currentFormIndex);
-            StartCoroutine(FormChangeCooldown());
-        }
-    }
 
-    void ActivateForm(int index)
-    {
-        for (int i = 0; i < forms.Length; i++)
-        {
-            forms[i].SetActive(i == index);
-        }
-    }
-
-    IEnumerator FormChangeCooldown()
-    {
-        canChangeForm = false;
-        yield return new WaitForSeconds(cooldownDuration);
-        canChangeForm = true;
-    }
-}
-
-/*public class ShapeShifting : MonoBehaviour, i_Update
-{
-    private void Start() { UpdateManager.Instance.RegisterUpdate(this); }
-    private void OnDisable() { UpdateManager.Instance.UnregisterUpdate(this); }
-    public void CustomUpdate()
-    {
-        if (BoolControler.Instance.isFire) { FireFormEnabled(); }
-        if (BoolControler.Instance.isWater) { WaterFormEnabled(); }
-        if (BoolControler.Instance.isNature) { NatureFormEnabled(); }
     }
     public void FireFormEnabled()
     {
@@ -79,4 +54,61 @@ public class FormChanger : MonoBehaviour
     {
 
     }
-}*/
+
+    void ActivateForm(int index)
+    {
+        for (int i = 0; i < forms.Length; i++)
+        {
+            forms[i].SetActive(i == index);
+        }
+    }
+
+    void ChangeForm(int formIndex)
+    {
+        if (formIndex != currentFormIndex)
+        {
+            forms[formIndex].gameObject.SetActive(true);
+            forms[currentFormIndex].gameObject.SetActive(false);
+
+            currentFormIndex = formIndex;
+            ActivateForm(currentFormIndex);
+            StartCoroutine(FormChangeCooldown());
+
+            switch (formIndex)
+            {
+                case 0:
+                    BoolControler.Instance.isNormal = true;
+                    BoolControler.Instance.isFire = false;
+                    BoolControler.Instance.isWater = false;
+                    BoolControler.Instance.isNature = false;
+                    break;
+                case 1:
+                    BoolControler.Instance.isNormal = false;
+                    BoolControler.Instance.isFire = true;
+                    BoolControler.Instance.isWater = false;
+                    BoolControler.Instance.isNature = false;
+                    break;
+                case 2:
+                    BoolControler.Instance.isNormal = false;
+                    BoolControler.Instance.isFire = false;
+                    BoolControler.Instance.isWater = true;
+                    BoolControler.Instance.isNature = false;
+                    break;
+                case 3:
+                    BoolControler.Instance.isNormal = false;
+                    BoolControler.Instance.isFire = false;
+                    BoolControler.Instance.isWater = false;
+                    BoolControler.Instance.isNature = true;
+                    break;
+            }
+        }
+    }
+
+    IEnumerator FormChangeCooldown()
+    {
+        canChangeForm = false;
+        yield return new WaitForSeconds(cooldownDuration);
+        canChangeForm = true;
+    }
+
+}
