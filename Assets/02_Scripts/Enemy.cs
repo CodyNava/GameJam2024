f01f4e086f7f2b26afcd3995f0563a81b5dac330
MikoHeine <Miko.Heine@games-academy.de>
2024-11-09 13:33:28 +0100

    Created Enemy Movement

--- Assets/02_Scripts/Enemy.cs
+++ Assets/02_Scripts/Enemy.cs
@@ -2,20 +2,70 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour, i_Update
 {
+    [Header("Stats")]
     public float maxHealth;
     public float currentHealth;
     public float damage;
-    public bool isDead;
     public float deathTime;
-    private void Start() { UpdateManager.Instance.RegisterUpdate(this); }
+    [Header("Movement")]
+    public float detectionRadius;
+    public float moveSpeed;
+    public float chargeSpeed;
+    public float chargeDelay;
+    public float chargeDistance;
+    public float chargeCoolDown;
+    [Header("Bools")]
+    public bool chargeReady;
+    public bool isDead;
+    public bool isCharging;
+    private Transform playerTransform;
+    private Vector2 chargeTarget;
+    private void Start() { UpdateManager.Instance.RegisterUpdate(this); currentHealth = maxHealth; playerTransform = GameObject.FindGameObjectWithTag("Player").transform; }
     private void OnDisable() { UpdateManager.Instance.UnregisterUpdate(this); }
-
     public void CostumUpdate()
     {
         if (currentHealth <= 0f)
         {
             Dying();
         }
+        else
+        {
+            DetectAndChargePlayer();
+        }
+    }
+    private void DetectAndChargePlayer()
+    {
+        if (playerTransform != null && !isCharging)
+        {
+            float distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
+
+            if (distanceToPlayer <= detectionRadius && distanceToPlayer > chargeDistance)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, playerTransform.position, moveSpeed * Time.deltaTime);
+            }
+            else if (distanceToPlayer <= chargeDistance)
+            {
+                chargeTarget = playerTransform.position;
+                if (chargeReady)
+                {
+                    StartCoroutine(PauseAndCharge());
+                }
+            }
+        }
+    }
+    private System.Collections.IEnumerator PauseAndCharge()
+    {
+        isCharging = true;
+        chargeReady = false;
+        yield return new WaitForSeconds(chargeDelay);
+        while (Vector2.Distance(transform.position, chargeTarget) > 0.1f)
+        {
+            transform.position = Vector2.Lerp(transform.position, chargeTarget, chargeSpeed * Time.deltaTime);
+            yield return null;
+        }
+        isCharging = false;
+        yield return new WaitForSeconds(chargeCoolDown);
+        chargeReady = true;
     }
     public void OnCollisionEnter2D(Collision2D collision)
     {
@@ -31,7 +81,14 @@ public class Enemy : MonoBehaviour, i_Update
     }
     public void DealDamage()
     {
-        PlayerStats.Instance.currentHealth -= damage;
+        if (isCharging)
+        {
+            PlayerStats.Instance.currentHealth -= damage * 2;
+        }
+        else
+        {
+            PlayerStats.Instance.currentHealth -= damage;
+        }
     }
     public void Dying()
     {
--- Assets/02_Scripts/Pickup.cs
+++ Assets/02_Scripts/Pickup.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
     private int elementToPickUp = 0;
-    
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.tag == "Player")
