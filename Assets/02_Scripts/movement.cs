ba3d91ae04fb2ff59cb04e86aa693bbb8f2c104f
Elija <eli.schmuck@gmx.net>
2024-11-09 09:38:17 +0100

    Movement Animations

--- Assets/02_Scripts/movement.cs
+++ Assets/02_Scripts/movement.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour,i_Update
+
+    
 {
+    [Header("Animator")]
+    public Animator running;
+    [Header("Movement")]
     public float speed = 5f;          // Geschwindigkeit des Spielers
     private Rigidbody2D rb;               // Referenz zum Rigidbody2D des Spielers
     private Vector2 movement;             // Bewegungsvektor
@@ -16,14 +21,7 @@ public class PlayerMovement : MonoBehaviour,i_Update
 
    public void CostumUpdate()
     {
-        //moveSpeed
-        float horizontalInput = Input.GetAxisRaw("Horizontal"); 
-        float vertiacalInput = Input.GetAxisRaw("Vertical");
-        
-        Vector3 inputVector = new Vector3(horizontalInput, vertiacalInput, 0); 
-        float inputStrengthClamped = Mathf.Min(inputVector.magnitude, 1); 
-        inputVector = inputVector.normalized * inputStrengthClamped;
-        transform.position += inputVector * Time.deltaTime * speed;
+       OnMove();
       
     }
 
@@ -31,5 +29,16 @@ public class PlayerMovement : MonoBehaviour,i_Update
     {
         // Bewegung anwenden
         rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+    }
+    public void OnMove()
+    {
+        //moveSpeed
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float vertiacalInput = Input.GetAxisRaw("Vertical");
+
+        running.SetFloat("speed", Mathf.Abs(vertiacalInput + horizontalInput));
+
+        Vector3 inputVector = new Vector3(horizontalInput, vertiacalInput, 0);
+        transform.position += inputVector * Time.deltaTime * speed;
     }
 }
